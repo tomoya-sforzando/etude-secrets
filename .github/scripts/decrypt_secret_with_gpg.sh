e667c646b667734a07b52050930f3187ef636ad2
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-11-13 18:00:33 +0900

    :bug: Fix path of secret file

--- .github/scripts/decrypt_secret_with_gpg.sh
+++ .github/scripts/decrypt_secret_with_gpg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-secret_txt=$HOME/with_gpg/secret.txt
-secret_txt_gpg=$HOME/with_gpg/secret.txt.gpg
+secret_txt=with_gpg/secret.txt
+secret_txt_gpg=with_gpg/secret.txt.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_SECRET_PASSPHRASE" \
 --output $secret_txt $secret_txt_gpg
